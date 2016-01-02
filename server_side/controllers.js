e010209473a7def35b0d25d714dd2f2db01e1cbb
Tony <david.guerrero87@live.com>
2016-01-01 20:07:22 -0800

    models created

--- server_side/controllers.js
+++ server_side/controllers.js
@@ -8,7 +8,12 @@ module.exports = {
     create: function(req, res){
       if(req.body)
       {
-        var patient = new db.Patient(req.body)
+        console.log(req.body, req.files)
+        var patient = new db.Patient()
+        patient.fname = req.body.fname
+        patient.email = req.body.email
+        patient.password = req.body.password
+        patient.recImg.data = fs.readFileSync(req.body.recImg)
         patient.save(function(err){
           if(err) res.json({error: err.message, success: false})
           res.json({message: 'Patient Added!', success: true})
